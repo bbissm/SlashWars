5e7ae5c3b374057726d5247cc162925099acd9f6
Martin Bissiger <martinbissiger@martins-macbook-pro.home>
2020-09-09 22:15:02 +0200

    First commit

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	public Rigidbody rb;
+    // Start is called before the first frame update
+	public float ForwardForce = 750f;
+	public float SideForce = 100f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // rb.AddForce(0,0,1000 * Time.deltaTime); //1000 on the z axis
+
+        if (Input.GetKey("d")) {
+        	rb.AddForce(SideForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        } 
+
+        if(Input.GetKey("a")) {
+        	rb.AddForce(-SideForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("w")) {
+        	rb.AddForce(0,0,ForwardForce * Time.deltaTime);
+        }
+
+        if(Input.GetKey("s")) {
+        	rb.AddForce(0,0,-ForwardForce * Time.deltaTime);
+        }
+
+        if(rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
