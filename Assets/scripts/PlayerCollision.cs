c4d541da0ac5206d73f3d852189433e45d5c9e8d
martin <martin.ivanenko@hotmail.de>
2021-10-28 14:18:43 +0200

    updated slash wars

---
+++ Assets/scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	// public int health = 1000;
+	public PlayerMove movement;
+
+	void OnCollisionEnter (Collision collisionInfo){
+			// public PlayerMove movement;
+
+		if(collisionInfo.collider.CompareTag("Enemy")){
+			//health = health - 100;
+			movement.enabled = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	public Transform player;
+	public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {    	// Debug.Log(position);
+    	scoreText.text = player.position.z.ToString("0");
+    	// Debug.Log(scoreText.text);
+    	// scroreText.Text = position;
+
+    }
+}
